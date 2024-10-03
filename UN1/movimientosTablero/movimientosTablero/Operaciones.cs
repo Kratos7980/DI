5abdc7b1af0388771c3249bed6afdde55fcb885a
Kratos7980 <cgarciav17@educastillalamancha.es>
2024-10-03 14:43:58 +0200

    093/10/2024

---
+++ UN1/movimientosTablero/movimientosTablero/Operaciones.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace movimientosTablero
+{
+    internal class Operaciones
+    {
+        // Rellenar la matriz con una cadena dada
+        public static void rellenarMatriz(string[,] tablero, string cadena)
+        {
+            for (int i = 0; i < tablero.GetLength(0); i++)
+            {
+                for (int j = 0; j < tablero.GetLength(1); j++)
+                {
+                    tablero[i, j] = cadena;
+                }
+            }
+        }
+
+        public static void ponerPosicion(string[,] tablero, int posI, int posJ, string cadena)
+        {
+            tablero[posI, posJ] = cadena;
+        }
+
+        public static void entradaOpcion(string[,] tablero, int posI, int posJ)
+        {
+            try
+            {
+                #region InicializaciónVariables
+                String opcion;
+                int opcionNum = 0;
+                bool salir = false;
+                #endregion
+                #region logicalWhile
+                while (!salir)
+                {
+                    mostrarOpciones();
+                    opcion = Console.ReadLine();
+
+                    if (Int32.TryParse(opcion, out opcionNum))
+                    {
+                        switch (opcionNum)
+                        {
+                            case 1:
+                                tablero[posI, posJ] = "X";
+                                posJ++;
+                                tablero[posI, posJ] = "0";
+                                break;
+                            case 2:
+                                break;
+                            case 3:
+                                break;
+                            case 4:
+                                break;
+                            case 5:
+                                salir = true;
+                                break;
+                            default:
+                                Console.WriteLine("Solo opciones entre 1 y 5");
+                                Console.ReadKey();
+                                break;
+                        }
+                    }
+                    mostrarMatriz(tablero);
+                }
+                #endregion
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Se ha producido un error " + e.ToString());
+                Console.ReadKey();
+            }
+        }
+
+        public static void mostrarOpciones()
+        {
+            Console.WriteLine("1.Derecha ");
+            Console.WriteLine("2.Izquierda");
+            Console.WriteLine("3.Abajo");
+            Console.WriteLine("4.Arriba");
+            Console.WriteLine("5.Salir");
+        }
+
+        public static void mostrarMatriz(string[,] matriz)
+        {
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                Console.Write(matriz[i, 0] + " ");
+                for (int j = 1; j < matriz.GetLength(1); j++)
+                {
+                    Console.Write(matriz[j, 1] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
