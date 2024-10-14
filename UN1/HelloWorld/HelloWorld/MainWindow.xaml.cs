35ec014c33213fa2ae273aa69d134831ba71ee18
Kratos7980 <cgarciav17@educastillalamancha.es>
2024-10-14 12:53:03 +0200

    14/10/2024

---
+++ UN1/HelloWorld/HelloWorld/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_Hellow(object sender, RoutedEventArgs e)
+        {
+            HellowLabel.Content = "Hellow World";
+            HellowLabel.Foreground = Brushes.Green;
+        }
+
+        private void Button_Click_Bye(object sender, RoutedEventArgs e)
+        {
+            HellowLabel.Content = "Bye World";
+            HellowLabel.Foreground = Brushes.Turquoise;
+        }
+    }
+}
