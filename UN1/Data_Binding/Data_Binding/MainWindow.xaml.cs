3cf1e446a6bd192ab48ee51c6fed89470066f908
Kratos7980 <cgarciav17@educastillalamancha.es>
2024-10-22 12:23:22 +0200

    s

---
+++ UN1/Data_Binding/Data_Binding/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Data_Binding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Big_Click(object sender, RoutedEventArgs e)
+        {
+            MiSlide.Value = 20;
+        }
+
+        private void Samall_Click(object sender, RoutedEventArgs e)
+        {
+            MiSlide.Value = 10;
+        }
+
+        private void Medium_Click(object sender, RoutedEventArgs e)
+        {
+            MiSlide.Value = 15;
+        }
+    }
+}
