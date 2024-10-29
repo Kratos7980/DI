5744c4b5a46405065c7c3a7958b70b70c66f6d8b
CarlGarcia97 <carlosvigar97@gmail.com>
2024-10-29 18:55:20 +0100

    ads

---
+++ UN1/MarioBross_wpf/MarioBross_wpf/domain/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioBross_wpf.domain
+{
+    internal class Player
+    {
+        private int lives {  get; set; }
+        private String name;
+        private int potions {  get; set; }
+
+        public Player(string name, int lives)
+        {
+            this.lives = lives;
+            this.name = name;
+            this.potions = 0;
+        }
+
+    }
+}
