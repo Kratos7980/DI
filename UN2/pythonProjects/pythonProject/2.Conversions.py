157021a08556a755c208a15597ad6dcd09e6092a
Kratos7980 <cgarciav17@educastillalamancha.es>
2024-11-06 15:27:11 +0100

    iuh

---
+++ UN2/pythonProjects/pythonProject/2.Conversions.py
@@ -0,0 +1,23 @@
+# implicits conversion
+"""
+num1 = 20
+num2 = 30.5
+
+print(type(num1), type(num2))
+
+num1 = num1 + num2
+
+print(num1,type(num1))
+"""
+
+#Explicit conversions
+
+num1 = 5.8
+print(num1, type(num1))
+
+num2 = int(num1)
+print(num2,type(num2))
+
+edad = int(input("Type your age: "))
+print(type(edad))
+
---
+++ UN2/pythonProjects/pythonProject/Print_String.py
@@ -0,0 +1,9 @@
+#print
+print("Hola mundo")
+print("Hola mundo te digo 'hola'")
+print(100+50)
+print("100+50")
+
+#Strings
+print("Hola"+" "+"Marta")
+print("Me llamo \"Carlos\"")
