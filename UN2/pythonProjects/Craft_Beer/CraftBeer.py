100e74ba3f01570a6eea4c5f1471346700958963
Kratos7980 <cgarciav17@educastillalamancha.es>
2024-11-07 08:56:37 +0100

    sd

--- UN2/pythonProjects/Craft_Beer/CraftBeer.py
+++
@@ -1,4 +0,0 @@
-pregunta1 = input("What is your name?:")
-pregunta2 = input("What is your favorite drink?:")
-
-print("Your beer is "+pregunta1+pregunta2)
--- UN2/pythonProjects/pythonProject/Print_String.py
+++
@@ -1,9 +0,0 @@
-#print
-print("Hola mundo")
-print("Hola mundo te digo 'hola'")
-print(100+50)
-print("100+50")
-
-#Strings
-print("Hola"+" "+"Marta")
-print("Me llamo \"Carlos\"")
