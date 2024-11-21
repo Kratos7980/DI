1bfa4ca00a0200661806ebbc203b92c02177d137
Kratos7980 <cgarciav17@educastillalamancha.es>
2024-11-21 13:49:11 +0100

    s

---
+++ UN2/Control_Excepciones_c#/Control_Excepciones_c#/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Control_Excepciones_c_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double[] listDiv = new double[10];
+            int dividendo = 20;
+            int divisor = 0;
+
+            while(divisor < 11)
+            {
+                try
+                {
+                    listDiv[divisor] = (dividendo / divisor);
+                    divisor++;
+                    dividendo--;
+                }
+                catch (DivideByZeroException e)
+                {
+                    Console.WriteLine("ERROR:Linea 22, NO se puede dividir entre 0, la variable divisor deber ser diferente a 0");
+                    divisor++;
+                    continue;
+                }catch(IndexOutOfRangeException e)
+                {
+                    Console.WriteLine("ERROR: Linea 22, La lista de divisiones no puede ser mayor de 10, intentas meter + de 10 registros");
+                    break;
+                }
+                
+            }
+        }
+    }
+}
---
+++ UN2/Control_Excepciones_c#/Control_Excepciones_c#/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Control_Excepciones_c#")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Control_Excepciones_c#")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("99e57575-c2cb-436d-955b-89d186e015bf")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
