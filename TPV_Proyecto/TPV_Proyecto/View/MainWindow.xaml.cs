8b002ee12a44041bfee0780b0a96c38bc0afebe5
Kratos7980 <cgarciav17@educastillalamancha.es>
2024-12-02 16:47:34 +0100

    d

---
+++ TPV_Proyecto/TPV_Proyecto/View/MainWindow.xaml.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TPV_Proyecto
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        // Control botones panel números.
+        private void btn7_Click(object sender, RoutedEventArgs e)
+        {
+            if (textNumber.Text.Contains("0.0"))
+            {
+                textNumber.Text = "";
+            }
+            textNumber.Text = textNumber.Text + "7";
+        }
+
+        private void btn8_Click(object sender, RoutedEventArgs e)
+        {
+            if (textNumber.Text.Contains("0.0"))
+            {
+                textNumber.Text = "";
+            }
+            textNumber.Text = textNumber.Text + "8";
+        }
+
+        private void btn9_Click(object sender, RoutedEventArgs e)
+        {
+            if (textNumber.Text.Contains("0.0"))
+            {
+                textNumber.Text = "";
+            }
+            textNumber.Text = textNumber.Text + "9";
+        }
+
+        private void btn4_Click(object sender, RoutedEventArgs e)
+        {
+            if (textNumber.Text.Contains("0.0"))
+            {
+                textNumber.Text = "";
+            }
+            textNumber.Text = textNumber.Text + "4";
+        }
+
+        private void btn5_Click(object sender, RoutedEventArgs e)
+        {
+            if (textNumber.Text.Contains("0.0"))
+            {
+                textNumber.Text = "";
+            }
+            textNumber.Text = textNumber.Text + "5";
+        }
+
+        private void btn6_Click(object sender, RoutedEventArgs e)
+        {
+            if (textNumber.Text.Contains("0.0"))
+            {
+                textNumber.Text = "";
+            }
+            textNumber.Text = textNumber.Text + "6";
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            if (textNumber.Text.Contains("0.0"))
+            {
+                textNumber.Text = "";
+            }
+            textNumber.Text = textNumber.Text + "1";
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            if (textNumber.Text.Contains("0.0"))
+            {
+                textNumber.Text = "";
+            }
+            textNumber.Text = textNumber.Text + "2";
+        }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+            if (textNumber.Text.Contains("0.0"))
+            {
+                textNumber.Text = "";
+            }
+            textNumber.Text = textNumber.Text + "3";
+        }
+
+        private void btn0_Click(object sender, RoutedEventArgs e)
+        {
+            if (textNumber.Text.Contains("0.0"))
+            {
+                textNumber.Text = "";
+            }
+            textNumber.Text = textNumber.Text + "0";
+        }
+
+        private void btnComa_Click(object sender, RoutedEventArgs e)
+        {
+            if (textNumber.Text.Contains("0.0"))
+            {
+                textNumber.Text = "";
+            }
+            textNumber.Text = textNumber.Text + ",";
+        }
+
+        private void btnMulti_Click(object sender, RoutedEventArgs e)
+        {
+            if (textNumber.Text.Contains("0.0"))
+            {
+                textNumber.Text = "";
+            }
+            textNumber.Text = textNumber.Text + "X";
+        }
+
+        private void btnBorrar_Click(object sender, RoutedEventArgs e)
+        {
+            textNumber.Text = "0.0";
+        }
+
+        
+    }
+}
